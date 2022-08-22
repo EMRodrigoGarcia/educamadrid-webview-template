4366f2e0882ec3ba50868c8c101a18e297b1725e
Rodrigo García López <rglopez300@gmail.com>
2022-08-22 13:13:26 +0200

    fixed statusbar

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from "react-native";
+import { WebView } from "react-native-webview";
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
 
 export default function App() {
+  // TODO: buscar manera de hacer statusbar solido en vez de usar view vacio (chapuza)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.statusbar} />
       <StatusBar style="auto" />
+      <WebView
+        style={styles.webview}
+        source={{ uri: "https://www.educa2.madrid.org/educamadrid/" }}
+      />
     </View>
   );
 }
@@ -13,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  webview: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+    paddingTop: Constants.statusBarHeight,
+  },
+
+  statusbar: {
+    height: Constants.statusBarHeight,
   },
 });
---
+++ README.md
@@ -0,0 +1,10 @@
+# EducaMadrid WebView App Template
+
+Simple template for creating WebView apps with Expo and React Native
+
+## How to use
+
+- Download npm and Expo
+- Clone repo
+- expo start:android (or expo start and pres "a" on the cli menu)
+- Done!
